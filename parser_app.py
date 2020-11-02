ccdb0cf23c50aae95848e035fed90d93d52e3c93
Eggoser <titanicsv@yandex.ru>
2020-11-02 19:24:50 +0300

    4.0P

--- parser_app.py
+++ parser_app.py
@@ -1,9 +1,3 @@
-#!/var/www/parser_app/venv/bin/python3
-import sys
-import logging
-logging.basicConfig(stream=sys.stderr)
-sys.path.insert(0,"/var/www/parser_app/")
-
 from app.app import app as application
 application.secret_key = 'kj43h2hdiushf243iurhakjdsfhKJDSFHIU3r4h23rhaksdjlfyl234019DSFlas'
 
